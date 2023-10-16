dd6b1d3e6f093dc3c82ad0e002f4d0f3d5d40a8b
progax01 <anurag35412@gmail.com>
2023-10-16 08:59:10 +0530

    updated

--- src/section/Upload.jsx
+++ src/section/Upload.jsx
@@ -4,9 +4,13 @@ import { Container, TextField, TextareaAutosize, Button } from '@mui/material';
 import { create as ipfsHttpClient } from 'ipfs-http-client';
 import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import Layout from "../component/Layout";
+import axios from 'axios'; 
 
-const client = ipfsHttpClient('https://ipfs.infura.io:5001/49ee4a00bfc33ea5df71/v0');
- 
+const client = ipfsHttpClient('https://api.pinata.cloud/pinning/pinataipfs');
+const key="49ee4a00bfc33ea5df71";
+const secretkey= "4158240fa73f2ae3dddc47ee3ad3adc3eaaa27cece3423ccaae1e57b7ce9355b";
+
 const Upload = ({ marketplace, nft }) => {
     const [image, setImage] = useState('');
     const [price, setPrice] = useState(null);
@@ -42,19 +46,19 @@ const Upload = ({ marketplace, nft }) => {
       }
     };
   
-    const mintThenList = async (result) => {
-      const uri = `https://ipfs.infura.io/ipfs/${result.path}`;
-      // mint NFT
-      await (await nft.mint(uri)).wait();
-      // get tokenId of new NFT
-      const id = await nft.tokenCount();
-      // approve marketplace to spend NFT
-      await (await nft.setApprovalForAll(marketplace.address, true)).wait();
-      // add NFT to the marketplace
-      const listingPrice = ethers.utils.parseEther(price.toString());
-      await (await marketplace.makeItem(nft.address, id, listingPrice)).wait();
-      toast.success("NFT created and listed successfully.");
-    };
+    // const mintThenList = async (result) => {
+    //   const uri = `https://ipfs.infura.io/ipfs/${result.path}`;
+    //   // mint NFT
+    //   await (await nft.mint(uri)).wait();
+    //   // get tokenId of new NFT
+    //   const id = await nft.tokenCount();
+    //   // approve marketplace to spend NFT
+    //   await (await nft.setApprovalForAll(marketplace.address, true)).wait();
+    //   // add NFT to the marketplace
+    //   const listingPrice = ethers.utils.parseEther(price.toString());
+    //   await (await marketplace.makeItem(nft.address, id, listingPrice)).wait();
+    //   toast.success("NFT created and listed successfully.");
+    // };
   
     return (
         <Layout>
